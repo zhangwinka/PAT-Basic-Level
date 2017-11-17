4a2e8a520b5eaccd30b85f3c5b49130234ae87ac
zhangwinka <32162099+zhangwinka@users.noreply.github.com>
2017-11-17 16:48:35 +0800

    Create Main.java

---
+++ 2/Main.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String n = scanner.nextLine();
+		scanner.close();
+		
+		int sum = 0;
+		for(int i = 0; i < n.length(); i++) {
+			sum += n.charAt(i) - '0';
+		}
+		
+		Stack<String> stack = new Stack<String>();
+		do {
+			switch (sum % 10) {
+			case 0:
+				stack.push("ling");
+				break;
+			case 1:
+				stack.push("yi");
+				break;
+			case 2:
+				stack.push("er");
+				break;
+			case 3:
+				stack.push("san");
+				break;
+			case 4:
+				stack.push("si");
+				break;
+			case 5:
+				stack.push("wu");
+				break;
+			case 6:
+				stack.push("liu");
+				break;
+			case 7:
+				stack.push("qi");
+				break;
+			case 8:
+				stack.push("ba");
+				break;
+			case 9:
+				stack.push("jiu");
+				break;
+			}
+			sum /= 10;
+		} while(sum != 0);
+		
+		System.out.print(stack.pop());
+		while(!stack.isEmpty()) {
+			System.out.println(" " + stack.pop());
+		}
+	}
+}
